629f3d91ed7bd71e13b95bbcc9b7387ed65c09fc
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2025-06-05 01:11:18 -0300

    atualização do projeto
    - definir uma classe separada ContaBancaria da classe principal para melhor organização e encapsulamento.

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,34 @@
+import br.com.alura.contabancaria.model.ContaBancaria;
 import java.util.Scanner;
 
-public class ContaBancaria {
+public class Main {
     public static void main(String[] args) {
-        String nome = "Guilherme";
-        String tipoConta = "Corrente";
-        double saldo = 3200.00;
-        double emprestimo = 0;
-        boolean emprestimoDisponivel = true;
-        int option = 0;
         Scanner scanner = new Scanner(System.in);
-        String menu = """
-                ** Digite a sua opção **
-                1 - Ver saldo
+        ContaBancaria conta = new ContaBancaria();
+        conta.setNome("Guilherme");
+        conta.setTipoDeConta("Corrente");
+        System.out.println("Bem-vindo " + conta.getNome() + " , escolha uma das opções da sua conta " + conta.getTipoDeConta() + " abaixo:");
+        conta.setMenu("""
+                ********** Digite a sua opção **********
+                1 - Ver informações da conta e saldo
                 2 - Transferir
                 3 - receber
                 4 - Empréstimo
                 5 - Sair
-                """;
+                """);
 
-        System.out.println("****************************");
-        System.out.println("\nNome do cliente: " + nome);
-        System.out.println("Tipo de conta: " + tipoConta);
-        System.out.println("Saldo atual: R$ " + saldo);
-        System.out.println("Empréstimo disponível: R$" + emprestimo);
-        System.out.println("\n****************************");
+        while (conta.option != 5) {
+            System.out.println(conta.getMenu());
+            conta.option = scanner.nextInt();
 
-        while (option != 5) {
-            System.out.println(menu);
-            option = scanner.nextInt();
-
-            switch (option) {
+            switch (conta.option) {
                 case 1:
-                    System.out.println("O saldo atualizado é de R$ " + saldo + " reais");
+                    System.out.println("****************************");
+                    System.out.println("\nNome do cliente: " + conta.getNome());
+                    System.out.println("Tipo de conta: " + conta.getTipoDeConta());
+                    System.out.println("Saldo atual: R$ " + conta.getSaldo());
+                    System.out.println("Empréstimo disponível: R$" + conta.getEmprestimo());
+                    System.out.println("\n****************************");
                     break;
                 case 2:
                     System.out.println("Quanto você vai transferir?");
@@ -40,12 +37,12 @@ public class ContaBancaria {
                     scanner.nextLine();
                     if (dinheiroDescontado < 0) {
                         System.out.println("Valor inválido!");
-                    } else if (dinheiroDescontado > saldo) {
+                    } else if (dinheiroDescontado > conta.getSaldo()) {
                         System.out.println("Saldo insuficiente!");
                     } else {
                         System.out.print("Nome do titular que vai receber a transferência: ");
                         String nomeDoRecebedor = scanner.nextLine();
-                        saldo -= dinheiroDescontado;
+                        conta.transferirDinheiro(dinheiroDescontado);
                         System.out.println("Transferência de R$ " + dinheiroDescontado + " reais para o titular " + nomeDoRecebedor + " realizado com sucesso");
                     }
                     break;
@@ -56,34 +53,34 @@ public class ContaBancaria {
                     if (valorAReceber < 0) {
                         System.out.println("Valor inválido!");
                     } else {
-                        saldo += valorAReceber;
+                        conta.sacarDinheiro(valorAReceber);
                         System.out.println("Você recebeu uma transferência de R$ " + valorAReceber + " reais");
                     }
                     break;
                 case 4:
-                    if (emprestimoDisponivel && emprestimo == 0) {
+                    if (conta.isEmprestimoDisponivel() && conta.getEmprestimo() == 0) {
                         System.out.print("Quanto é o seu salário atual por mês: ");
                         double salario = scanner.nextDouble();
-                        emprestimo = salario * 6;
-                        System.out.println("baseado no seu salário, você pode pedir até R$ " + emprestimo + " reais de empréstimo");
-                        System.out.println("Cada empréstimo que você fizer vai ser descontado do limite do teto previsto para o seu empréstimo que é de R$ " + emprestimo);
+                        conta.gerarEmprestismo(salario);
+                        System.out.println("baseado no seu salário, você pode pedir até R$ " + conta.getEmprestimo() + " reais de empréstimo");
+                        System.out.println("Cada empréstimo que você fizer vai ser descontado do limite do teto previsto para o seu empréstimo que é de R$ " + conta.getEmprestimo());
                     }
-                    if (!emprestimoDisponivel) {
+                    if (!conta.isEmprestimoDisponivel()) {
                         System.out.println("Empréstimo indisponível no momento, espere por seis meses para realizar um novo empréstimo");
                         break;
                     }
                     System.out.println("Quanto você quer receber?");
                     System.out.print("Valor: ");
                     double valorDoEmprestimo = scanner.nextDouble();
-                    if (valorDoEmprestimo > emprestimo) {
+                    if (valorDoEmprestimo > conta.getEmprestimo()) {
                         System.out.println("Valor Acima do limite, coloque um valor abaixo do limite disponível!");
-                        System.out.println("Valor disponível: R$ " + emprestimo);
+                        System.out.println("Valor disponível: R$ " + conta.getEmprestimo());
                     } else {
-                        emprestimo -= valorDoEmprestimo;
-                        saldo += valorDoEmprestimo;
-                        System.out.println("Empréstimo realizado com sucesso, empréstimo disponível agora é de R$ " + emprestimo);
-                        if (emprestimo == 0) {
-                            emprestimoDisponivel = false;
+                        conta.sacarEmprestimo(valorDoEmprestimo);
+                        conta.sacarDinheiro(valorDoEmprestimo);
+                        System.out.println("Empréstimo realizado com sucesso, empréstimo disponível agora é de R$ " + conta.getEmprestimo());
+                        if (conta.getEmprestimo() == 0) {
+                            conta.setEmprestimoDisponivel(false);
                             System.out.println("você chegou no limite previsto de empréstimos, e ele estará indisponível por seis meses");
                         }
                     }
---
+++ src/br/com/alura/contabancaria/model/ContaBancaria.java
@@ -0,0 +1,71 @@
+package br.com.alura.contabancaria.model;
+
+public class ContaBancaria {
+    private String nome;
+    private String tipoDeConta;
+    private double saldo = 0.00;
+    private double emprestimo = 0.00;
+    private boolean emprestimoDisponivel = true;
+    public int option = 0;
+    private String menu;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setTipoDeConta(String tipoDeConta) {
+        if (tipoDeConta.equalsIgnoreCase("corrente") || tipoDeConta.equalsIgnoreCase("poupança")) {
+            this.tipoDeConta = tipoDeConta;
+        } else {
+            System.out.println("Erro: Escolha entre conta corrente ou conta poupança, por favor!");
+        }
+    }
+
+    public String getTipoDeConta() {
+        return tipoDeConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public double getEmprestimo() {
+        return emprestimo;
+    }
+
+    public void setMenu(String menu) {
+        this.menu = menu;
+    }
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public boolean isEmprestimoDisponivel() {
+        return emprestimoDisponivel;
+    }
+
+    public void setEmprestimoDisponivel(boolean emprestimoDisponivel) {
+        this.emprestimoDisponivel = emprestimoDisponivel;
+    }
+
+    public void transferirDinheiro(double valor) {
+        saldo -= valor;
+    }
+
+    public void sacarDinheiro(double valor) {
+        saldo += valor;
+    }
+
+    public void gerarEmprestismo(double valor) {
+        emprestimo = valor * 6;
+    }
+
+    public void sacarEmprestimo(double valor) {
+        emprestimo -= valor;
+    }
+}
